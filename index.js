89fe293684e3a9851bc455606d686252ae8ab5ea
Timileyin Oluwayomi <83769790+Timileyin105@users.noreply.github.com>
2022-05-17 13:39:47 +0100

    d
    d

--- index.js
+++ index.js
@@ -201,7 +201,7 @@ var intv = setInterval(async () => {
 }
 
 const startCrawller = async()=>{
-  const browser = await puppeteer.launch({headless: false, args: ['--no-sandbox']});
+  const browser = await puppeteer.launch({headless: true, args: ['--no-sandbox']});
   const crawler = await browser.newPage();
   await crawler.setUserAgent('Mozilla/5.0 (Windows NT 5.1; rv:5.0) Gecko/20100101 Firefox/5.0')
   var cookie = [ // cookie exported by google chrome plugin editthiscookie
@@ -249,12 +249,9 @@ var intv = setInterval(async()=>{
     clearInterval(intv)
     clearInterval(lintv)
     console.log('Cloudfare DDoS protection successfully bypassed')
-    // for (let link of movieLink){
-    //   scrapPage(browser, viewport, link)
-    // }
-    scrapPage(browser, viewport, movieLink[0])
-    console.log('arr is', movieLink)
-    
+    for (let link of movieLink){
+      scrapPage(browser, viewport, link)
+    }
   }else{
     console.log('cloudfare blocking connection: reconnecting....', loadingTimout)
     if(loadingTimout > 150){
